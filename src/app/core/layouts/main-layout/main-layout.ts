10d65c4ab1b9bfec979a8840cafc26237a0df323
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-05-13 18:34:47 +0200

    feat: add main layout to the app

--- src/app/core/layouts/main-layout/main-layout.ts
+++ src/app/core/layouts/main-layout/main-layout.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from '@core/layouts/footer/footer';
+import { Header } from '@core/layouts/header/header';
 
 @Component({
   selector: 'player-main-layout',
-  imports: [],
+  imports: [RouterOutlet, Footer, Header],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
