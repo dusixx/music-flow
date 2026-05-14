248abe40e1d6a3bfae08894a9891893637f1c117
Ina Litvinka <inlibitum@gmail.com>
2026-05-15 00:12:27 +0200

    feat: connect team data to about page

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TeamMember } from './about.models';
+import { TEAM_MEMBERS } from './about.data';
+import { TeamMemberCard } from './components/team-member-card/team-member-card';
 
 @Component({
   selector: 'player-about',
-  imports: [],
+  imports: [TeamMemberCard],
   templateUrl: './about.html',
   styleUrl: './about.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class About {}
+export class About {
+  readonly teamMembers: readonly TeamMember[] = TEAM_MEMBERS;
+}
