b440769476dd96e93e83f0a244dc3671a8925c10
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2026-05-14 17:03:40 +0200

    refactor: change links in the footer to a copyright text

--- src/app/core/layouts/footer/footer.ts
+++ src/app/core/layouts/footer/footer.ts
@@ -1,23 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatListItem, MatNavList } from '@angular/material/list';
-
-interface FooterLink {
-  name: string;
-  url: string;
-}
 
 @Component({
   selector: 'player-footer',
-  imports: [MatNavList, MatListItem],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Footer {
-  footerLinks: FooterLink[] = [
-    { name: 'RS School', url: 'https://rs.school/' },
-    { name: 'Ina', url: 'https://github.com/inalitvinka/' },
-    { name: 'Andrew', url: 'https://github.com/dusixx/' },
-    { name: 'Aleksey', url: 'https://github.com/nginit/' },
-  ];
+  private readonly copyrightYear = new Date().getFullYear();
+  readonly copyrightText = `© ${this.copyrightYear} MusicFlow. All Rights Reserved`;
 }
