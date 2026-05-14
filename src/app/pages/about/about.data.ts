b75f916472bb57cd1dadb06e2deafe447aa0fcee
Ina Litvinka <inlibitum@gmail.com>
2026-05-15 00:08:57 +0200

    feat: add mock data for about page

---
+++ src/app/pages/about/about.data.ts
@@ -0,0 +1,76 @@
+import { TeamMember } from './about.models';
+
+export const TEAM_MEMBERS: TeamMember[] = [
+  {
+    name: 'Ina',
+    role: 'Front-End Developer',
+    bio: 'Works on UI components and Angular architecture. Handles data flow and component communication.',
+    photo: 'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f',
+    github: 'https://github.com/inalitvinka',
+    favtracks: [
+      {
+        id: 'track-1',
+        title: 'While We Sleep',
+        artist: 'Insomnium',
+      },
+      {
+        id: 'track-2',
+        title: 'The Sound Of Silence',
+        artist: 'Disturbed',
+      },
+      {
+        id: 'track-3',
+        title: 'Smells Like Teen Spirit',
+        artist: 'Nirvana',
+      },
+    ],
+  },
+  {
+    name: 'Andrew',
+    role: 'Front-End Developer',
+    bio: 'Works on UI components and Angular architecture. Handles data flow and component communication.',
+    photo: 'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f',
+    github: 'https://github.com/dusixx',
+    favtracks: [
+      {
+        id: 'track-1',
+        title: 'While We Sleep',
+        artist: 'Insomnium',
+      },
+      {
+        id: 'track-2',
+        title: 'The Sound Of Silence',
+        artist: 'Disturbed',
+      },
+      {
+        id: 'track-3',
+        title: 'Smells Like Teen Spirit',
+        artist: 'Nirvana',
+      },
+    ],
+  },
+  {
+    name: 'Aleksei',
+    role: 'Front-End Developer',
+    bio: 'Works on UI components and Angular architecture. Handles data flow and component communication.',
+    photo: 'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f',
+    github: 'https://github.com/nginit',
+    favtracks: [
+      {
+        id: 'track-1',
+        title: 'While We Sleep',
+        artist: 'Insomnium',
+      },
+      {
+        id: 'track-2',
+        title: 'The Sound Of Silence',
+        artist: 'Disturbed',
+      },
+      {
+        id: 'track-3',
+        title: 'Smells Like Teen Spirit',
+        artist: 'Nirvana',
+      },
+    ],
+  },
+];
