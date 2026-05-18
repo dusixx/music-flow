2da9351e3f6237a179c738208f1783c34d9c6b96
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-18 04:28:59 +0300

    feat: add routerLink to the home button

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Button } from '@app/shared/directives/button/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'player-not-found',
-  imports: [Button],
+  imports: [Button, RouterLink],
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
