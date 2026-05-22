dfa2a4dce0f28a34b04bb57cb9bdd6e184427d80
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-22 15:32:50 +0300

    feat: setup 3 routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from '@core/guards/guest/guest-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('@pages/login/login').then((m) => m.Login),
+    canActivate: [guestGuard],
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('@pages/register/register').then((m) => m.Register),
+    canActivate: [guestGuard],
+  },
+  {
+    path: '**',
+    loadComponent: () => import('@pages/not-found/not-found').then((m) => m.NotFound),
+  },
+];
