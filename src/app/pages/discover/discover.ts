7a842f4f3115b9a39c363f2b58330298ee80437a
Ina Litvinka <inlibitum@gmail.com>
2026-05-23 00:47:41 +0200

    feat: initialize discover page with basic content

---
+++ src/app/pages/discover/discover.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'player-discover',
+  imports: [],
+  templateUrl: './discover.html',
+  styleUrl: './discover.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Discover {}
