5167d068bd8702f8e08eaa6af92410f4618e9c69
Ina Litvinka <inlibitum@gmail.com>
2026-05-23 01:38:18 +0200

    feat: add new routes with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,23 @@ import { guestGuard } from '@core/guards/guest/guest-guard';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'discover',
+    pathMatch: 'full',
+  },
+  {
+    path: 'discover',
+    loadComponent: () => import('@pages/discover/discover').then((m) => m.Discover),
+  },
+  {
+    path: 'library',
+    loadComponent: () => import('@pages/library/library').then((m) => m.Library),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('@pages/about/about').then((m) => m.About),
+  },
+  {
     path: 'login',
     loadComponent: () => import('@pages/login/login').then((m) => m.Login),
     canActivate: [guestGuard],
