87df7f5e3d7bc2250d74a4a0e838fe8b89648978
dusixx <113414935+dusixx@users.noreply.github.com>
2026-05-26 21:34:06 +0300

    chore: replace register component with signup

---
+++ src/app/pages/signup/signup.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'player-signup',
+  imports: [],
+  templateUrl: './signup.html',
+  styleUrl: './signup.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Signup {}
