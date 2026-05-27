e582af4843697603d4ac0a5ec84752a85f1bb583
Ina Litvinka <inlibitum@gmail.com>
2026-05-27 20:20:04 +0200

    feat: replace mock service with firebase authentication

--- src/app/core/services/auth/auth.ts
+++ src/app/core/services/auth/auth.ts
@@ -1,17 +1,49 @@
 import { Injectable, signal } from '@angular/core';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { firebaseApp } from '@core/firebase/firebase.config';
+import { FirebaseError } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private readonly auth = getAuth(firebaseApp);
   private _isLoggedIn = signal(false);
   readonly isAuthenticated = this._isLoggedIn.asReadonly();
 
-  login() {
-    this._isLoggedIn.set(true);
+  constructor() {
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        console.log('user is signed in');
+        this._isLoggedIn.set(true);
+      } else {
+        console.log('user is signed out');
+        this._isLoggedIn.set(false);
+      }
+    });
   }
 
-  logout() {
-    this._isLoggedIn.set(false);
+  async login(email: string, password: string) {
+    try {
+      await signInWithEmailAndPassword(this.auth, email, password);
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        console.error(error.message);
+      } else {
+        console.error('login error:', error);
+      }
+    }
+  }
+
+  async logout() {
+    try {
+      await signOut(this.auth);
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        console.error(error.message);
+      } else {
+        console.error('logout error:', error);
+      }
+    }
   }
 }
