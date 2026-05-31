ffdf7c9ba7fcf23dc4df01eed24e24e1902ada6e
Ina Litvinka <inlibitum@gmail.com>
2026-05-31 03:05:38 +0200

    refactor: simplify redirect service

--- src/app/core/services/redirect/redirect-service.ts
+++ src/app/core/services/redirect/redirect-service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject, effect } from '@angular/core';
-import { Router, ActivatedRouteSnapshot } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth-service';
 import { REQUIRES_AUTH } from '@app/shared/constants/requires-auth.const';
 
@@ -20,22 +20,11 @@ export class RedirectService {
       if (state !== 'guest') {
         return;
       }
-      const routeRoot = this.router.routerState.snapshot.root;
-      if (this.hasRouteData(routeRoot, REQUIRES_AUTH)) {
+
+      const activeRoute = this.router.routerState.snapshot.root.firstChild;
+      if (activeRoute?.data?.[REQUIRES_AUTH]) {
         this.router.navigate(['/']);
       }
     });
-  }
-
-  private hasRouteData(route: ActivatedRouteSnapshot, key: string) {
-    if (route.data && route.data[key]) {
-      return true;
-    }
-    for (const child of route.children) {
-      if (this.hasRouteData(child, key)) {
-        return true;
-      }
-    }
-    return false;
   }
 }
