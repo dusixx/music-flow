7484b67c7842a1204bab44b77bfceab24ac5ac45
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-01 21:46:17 +0300

    feat: implement track cover

---
+++ src/app/shared/components/track-cover/track-cover.ts
@@ -0,0 +1,29 @@
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { Button } from '@app/shared/directives/button/button';
+import { Track } from '@app/shared/models/track';
+
+type TrackCoverVariant = 'primary' | 'secondary';
+
+@Component({
+  selector: 'player-track-cover',
+  imports: [Button],
+  templateUrl: './track-cover.html',
+  styleUrl: './track-cover.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.playing]': 'isPlaying()',
+    '[class.secondary]': 'variant() === "secondary"',
+    '[class.hovered]': 'isHovered()',
+  },
+})
+export class TrackCover {
+  track = input.required<Track>();
+  variant = input<TrackCoverVariant>('primary');
+
+  isHovered = input(false);
+  isPlaying = input(false);
+  isFavorite = input(false);
+
+  playClicked = output<Track>();
+  favoriteClicked = output<Track>();
+}
