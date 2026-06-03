ec7fc342b0ae8cb3ed3ac746f5af45cc903317be
Ina Litvinka <inlibitum@gmail.com>
2026-06-03 17:25:11 +0200

    refactor: get rid of comments

--- src/app/core/firebase/firebase.config.ts
+++ src/app/core/firebase/firebase.config.ts
@@ -1,13 +1,4 @@
 import { initializeApp } from 'firebase/app';
 import { environment } from '@env/environment';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCxsPXb5qjNJNSCnCyzNKEyFDT8nnTiT_Y',
-//   authDomain: 'music-flow-d66c2.firebaseapp.com',
-//   projectId: 'music-flow-d66c2',
-//   storageBucket: 'music-flow-d66c2.firebasestorage.app',
-//   messagingSenderId: '990998002274',
-//   appId: '1:990998002274:web:f28d7c8ef95ddda2f52f3b',
-// };
-
 export const firebaseApp = initializeApp(environment.firebase);
