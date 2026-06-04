ba8a8154c340cc24a5eedd15f88f719299ec37d5
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-04 09:18:00 +0300

    feat: add stop-propagation directive

---
+++ src/app/shared/directives/stop-propagation/stop-propagation.spec.ts
@@ -0,0 +1,8 @@
+import { StopPropagation } from './stop-propagation';
+
+describe('StopPropagation', () => {
+  it('should create an instance', () => {
+    const directive = new StopPropagation();
+    expect(directive).toBeTruthy();
+  });
+});
