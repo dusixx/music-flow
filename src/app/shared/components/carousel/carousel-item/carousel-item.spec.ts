d262345b750ab56f841fa616b28d149d9a3bcfeb
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-04 09:26:37 +0300

    feat: implement Carousel component

---
+++ src/app/shared/components/carousel/carousel-item/carousel-item.spec.ts
@@ -0,0 +1,8 @@
+import { CarouselItem } from './carousel-item';
+
+describe('CarouselItem', () => {
+  it('should create an instance', () => {
+    const directive = new CarouselItem();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/carousel/carousel.ts
@@ -0,0 +1,66 @@
+import {
+  afterNextRender,
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  ElementRef,
+  inject,
+  signal,
+} from '@angular/core';
+
+const EPSILON = 1;
+
+@Component({
+  selector: 'player-carousel',
+  imports: [],
+  templateUrl: './carousel.html',
+  styleUrl: './carousel.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(scroll)': 'updateScrollEdges()',
+  },
+})
+export class Carousel {
+  private host = inject<ElementRef<HTMLElement>>(ElementRef);
+  private destroyRef = inject(DestroyRef);
+
+  atStart = signal(true);
+  atEnd = signal(false);
+
+  constructor() {
+    this.initScrollTracking();
+  }
+
+  private initScrollTracking() {
+    afterNextRender(() => {
+      this.updateScrollEdges();
+
+      const ro = new ResizeObserver((_) => {
+        this.updateScrollEdges();
+      });
+      ro.observe(this.host.nativeElement);
+
+      this.destroyRef.onDestroy(() => ro.disconnect());
+    });
+  }
+
+  protected updateScrollEdges() {
+    const el = this.host.nativeElement;
+    this.atStart.set(el.scrollLeft <= EPSILON);
+    this.atEnd.set(el.scrollLeft + el.clientWidth >= el.scrollWidth - EPSILON);
+  }
+
+  next() {
+    const el = this.host.nativeElement;
+    el.scrollBy({
+      left: el.clientWidth,
+    });
+  }
+
+  prev() {
+    const el = this.host.nativeElement;
+    el.scrollBy({
+      left: -el.clientWidth,
+    });
+  }
+}
