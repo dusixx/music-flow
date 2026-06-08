48e3ed091f8c69e169bbfd7b6a8402901df28c4c
Ina Litvinka <inlibitum@gmail.com>
2026-06-08 21:53:03 +0200

    refactor: get rid of comments

--- src/app/shared/utils/firestore-data-converter.ts
+++ src/app/shared/utils/firestore-data-converter.ts
@@ -5,18 +5,6 @@ import {
   WithFieldValue,
 } from 'firebase/firestore';
 
-// export const firestoreDataConverter = <T>(): FirestoreDataConverter<T> => ({
-//   toFirestore: (data: WithFieldValue<T>): DocumentData => {
-//     return data as DocumentData;
-//   },
-//   fromFirestore: (snapshot: QueryDocumentSnapshot): T => {
-//     const data = snapshot.data();
-//     return {
-//       id: snapshot.id,
-//       ...data,
-//     } as T;
-//   },
-// });
 export function firestoreDataConverter<T>(): FirestoreDataConverter<T> {
   return {
     toFirestore(data: WithFieldValue<T>): DocumentData {
