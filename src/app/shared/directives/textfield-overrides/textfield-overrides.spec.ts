159c4ca96e6789c7990653843795ca581ca78906
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-12 22:45:38 +0300

    feat: add directive to sync  state of tui-textfield when working with signal forms

---
+++ src/app/shared/directives/textfield-overrides/textfield-overrides.spec.ts
@@ -0,0 +1,8 @@
+import { TextfieldOverrides } from './textfield-overrides';
+
+describe('TuiTextfieldOverride', () => {
+  it('should create an instance', () => {
+    const directive = new TextfieldOverrides();
+    expect(directive).toBeTruthy();
+  });
+});
