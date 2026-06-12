c1b7a725dc1294424661e25708abd11a003e3e75
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-12 22:39:09 +0300

    feat: implement FormTextfield based on tui-textfield

---
+++ src/app/shared/components/form-textfield/form-textfield.ts
@@ -0,0 +1,34 @@
+import { Component, computed, input } from '@angular/core';
+import { FieldTree, FormField } from '@angular/forms/signals';
+import { TextfieldOverrides } from '@app/shared/directives/textfield-overrides/textfield-overrides';
+import { TuiNativeValidator } from '@taiga-ui/cdk/directives/native-validator';
+import { TuiButtonX, TuiDropdown, TuiError, TuiIcon, TuiInput } from '@taiga-ui/core';
+import { TuiPassword } from '@taiga-ui/kit';
+import { Sprite } from '../sprite/sprite';
+
+@Component({
+  selector: 'player-form-textfield',
+  imports: [
+    FormField,
+    TuiInput,
+    TuiIcon,
+    TuiPassword,
+    TuiButtonX,
+    TuiError,
+    TuiNativeValidator,
+    TextfieldOverrides,
+    TuiDropdown,
+    Sprite,
+  ],
+  templateUrl: './form-textfield.html',
+  styleUrl: './form-textfield.scss',
+})
+export class FormTextfield {
+  formField = input.required<FieldTree<string>>();
+  invalid = input(false);
+  showErrorIcon = input(false);
+  type = input('text');
+  placeholder = input('');
+
+  protected passwordInput = computed(() => this.type() === 'password');
+}
