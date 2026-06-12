7c010be2b1f94340fab0b85a9a56489890de0b90
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-12 22:49:40 +0300

    chore: update icons sprite

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -1,10 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@app/core/services/auth/auth-service';
+import { getErrorMessage } from '@app/shared/utils/error.utils';
+import { SignupForm } from './components/signup-form/signup-form';
+import { SignupFormData } from './components/signup-form/signup-form.models';
 
 @Component({
   selector: 'player-signup',
-  imports: [],
+  imports: [SignupForm],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Signup {}
+export class Signup {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  protected error = signal('');
+  protected loading = signal(false);
+
+  async register(data: SignupFormData) {
+    const { email, password, name: displayName } = data;
+
+    this.loading.set(true);
+    try {
+      await this.authService.register({ email, password, displayName });
+      this.router.navigate(['/']);
+    } catch (error) {
+      this.error.set(getErrorMessage(error));
+    } finally {
+      this.loading.set(false);
+    }
+  }
+}
