eb25862ea53fd7a13280a953e1e34d74a6683a1d
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-13 02:35:40 +0300

    feat: update guest guard to full version

--- src/app/core/guards/guest/guest-guard.ts
+++ src/app/core/guards/guest/guest-guard.ts
@@ -1,10 +1,21 @@
 import { inject } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '@app/core/services/auth/auth-service';
+import { filter, map, take } from 'rxjs';
 
 export const guestGuard: CanActivateFn = () => {
   const router = inject(Router);
   const authService = inject(AuthService);
+  const authState = authService.authState;
 
-  return authService.isAuthenticated() ? router.parseUrl('/') : true;
+  if (authState() !== 'loading') {
+    return authState() === 'guest' ? true : router.createUrlTree(['/']);
+  }
+
+  return toObservable(authState).pipe(
+    filter((state) => state !== 'loading'),
+    take(1),
+    map((state) => (state === 'guest' ? true : router.createUrlTree(['/'])))
+  );
 };
