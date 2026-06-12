7f0e6fb9596c5fef90afe2d3e30e1b729576181a
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-12 22:41:33 +0300

    feat: add 'type' input for button

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, input } from '@angular/core';
 import { Sprite } from '../sprite/sprite';
 
 type ButtonVariant = 'base' | 'primary' | 'secondary';
 type ButtonSize = 'small' | 'medium';
+type ButtonType = HTMLButtonElement['type'];
 
 @Component({
   selector: 'button[playerButton]',
@@ -19,10 +20,19 @@ type ButtonSize = 'small' | 'medium';
     '[class.small]': 'size() === "small"',
     '[class.loading]': 'loading()',
     '[disabled]': 'disabled() || loading()',
+    '[type]': 'buttonType',
+    '[class]': 'playerButton()',
   },
 })
 export class Button {
+  private host = inject<ElementRef<HTMLButtonElement>>(ElementRef);
+
   protected spinnerPath = 'assets/images/spinner.gif';
+
+  playerButton = input<string>();
+
+  private type = this.host.nativeElement.getAttribute('type');
+  protected buttonType: ButtonType = this.type ? (this.type as ButtonType) : 'button';
 
   variant = input<ButtonVariant>('primary');
   size = input<ButtonSize>('medium');
