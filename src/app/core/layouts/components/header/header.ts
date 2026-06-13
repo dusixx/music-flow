631b0783df74c9f120bee570aee15860862e3a0a
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-13 06:09:18 +0300

    feat: update header so that user's initials appear on profile button

--- src/app/core/layouts/components/header/header.ts
+++ src/app/core/layouts/components/header/header.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   ElementRef,
   inject,
   model,
@@ -30,6 +31,17 @@ export class Header {
   private menuButton = viewChild.required<ElementRef<HTMLButtonElement>>('menuButton');
   protected searchQuery = '';
   menuOpen = model(false);
+
+  protected userNameInitial = computed(() => {
+    const user = this.authService.user();
+    if (!user) {
+      return;
+    }
+    console.debug('computed', user.displayName);
+
+    const name = user.displayName || user.email;
+    return name && name[0].toLocaleUpperCase();
+  });
 
   logout() {
     this.authService.logout();
