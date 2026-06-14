54611d01b406ba5f5f3540722d82b050f6b4e771
Ina Litvinka <inlibitum@gmail.com>
2026-06-14 18:44:20 +0200

    feat: update library component

--- src/app/pages/library/library.ts
+++ src/app/pages/library/library.ts
@@ -1,10 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, inject } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
+import { PlaylistCreate } from './components/playlist-create/playlist-create';
+import { Dialog } from '@app/shared/components/dialog/dialog';
 
 @Component({
   selector: 'player-library',
-  imports: [],
+  imports: [PlaylistCreate, Dialog, RouterOutlet],
   templateUrl: './library.html',
   styleUrl: './library.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Library {}
+export class Library {
+  private router = inject(Router);
+  protected isModalOpen = signal(false);
+
+  // protected isChildRouteActive = computed(() => !this.router.url.includes('playlist'));
+  protected isChildRouteActive = toSignal(
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      map((event) => !event.urlAfterRedirects.includes('playlist'))
+    ),
+    { initialValue: !this.router.url.includes('playlist') }
+  );
+
+  protected openCreateForm() {
+    this.isModalOpen.set(true);
+  }
+
+  protected closeCreateForm() {
+    this.isModalOpen.set(false);
+  }
+}
