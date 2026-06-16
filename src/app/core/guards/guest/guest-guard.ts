6594a705734bd6ff1bcc2368d0d3becbc6a78e06
Ina Litvinka <inlibitum@gmail.com>
2026-06-16 02:38:46 +0200

    refactor: get rid of comments

--- src/app/core/guards/guest/guest-guard.ts
+++ src/app/core/guards/guest/guest-guard.ts
@@ -2,25 +2,3 @@ import { CanActivateFn } from '@angular/router';
 import { createAuthGuard } from '../create-auth-guard';
 
 export const guestGuard: CanActivateFn = createAuthGuard('guest');
-
-// import { inject } from '@angular/core';
-// import { toObservable } from '@angular/core/rxjs-interop';
-// import { CanActivateFn, Router } from '@angular/router';
-// import { AuthService } from '@app/core/services/auth/auth-service';
-// import { filter, map, take } from 'rxjs';
-
-// export const guestGuard: CanActivateFn = () => {
-//   const router = inject(Router);
-//   const authService = inject(AuthService);
-//   const authState = authService.authState;
-
-//   if (authState() !== 'loading') {
-//     return authState() === 'guest' || router.createUrlTree(['/']);
-//   }
-
-//   return toObservable(authState).pipe(
-//     filter((state) => state !== 'loading'),
-//     take(1),
-//     map((state) => state === 'guest' || router.createUrlTree(['/']))
-//   );
-// };
