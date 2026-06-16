3b637b3caae0d2a968b4e86fa426fc042a7588e8
Ina Litvinka <inlibitum@gmail.com>
2026-06-16 02:38:09 +0200

    refactor: update guest guard

--- src/app/core/guards/guest/guest-guard.ts
+++ src/app/core/guards/guest/guest-guard.ts
@@ -1,21 +1,26 @@
-import { inject } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '@app/core/services/auth/auth-service';
-import { filter, map, take } from 'rxjs';
+import { CanActivateFn } from '@angular/router';
+import { createAuthGuard } from '../create-auth-guard';
 
-export const guestGuard: CanActivateFn = () => {
-  const router = inject(Router);
-  const authService = inject(AuthService);
-  const authState = authService.authState;
+export const guestGuard: CanActivateFn = createAuthGuard('guest');
 
-  if (authState() !== 'loading') {
-    return authState() === 'guest' || router.createUrlTree(['/']);
-  }
+// import { inject } from '@angular/core';
+// import { toObservable } from '@angular/core/rxjs-interop';
+// import { CanActivateFn, Router } from '@angular/router';
+// import { AuthService } from '@app/core/services/auth/auth-service';
+// import { filter, map, take } from 'rxjs';
 
-  return toObservable(authState).pipe(
-    filter((state) => state !== 'loading'),
-    take(1),
-    map((state) => state === 'guest' || router.createUrlTree(['/']))
-  );
-};
+// export const guestGuard: CanActivateFn = () => {
+//   const router = inject(Router);
+//   const authService = inject(AuthService);
+//   const authState = authService.authState;
+
+//   if (authState() !== 'loading') {
+//     return authState() === 'guest' || router.createUrlTree(['/']);
+//   }
+
+//   return toObservable(authState).pipe(
+//     filter((state) => state !== 'loading'),
+//     take(1),
+//     map((state) => state === 'guest' || router.createUrlTree(['/']))
+//   );
+// };
