6d6a2ede269db5271a22cb9ce83160166202bb96
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-19 10:06:49 +0300

    feat: implement jamendo service

---
+++ src/app/core/api/jamendo/jamendo-service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { JAMENDO_API_CONFIG } from './jamendo.provider';
+import { JamendoApiEndpoint, JamendoResponse } from './jamendo.types';
+
+type QueryParamValue = string | number | boolean | null | undefined;
+type QueryParams = Record<string, QueryParamValue | readonly QueryParamValue[]>;
+
+@Service()
+export class JamendoService {
+  private apiConfig = inject(JAMENDO_API_CONFIG);
+  private httpClient = inject(HttpClient);
+
+  private buildUrl(endpoint: JamendoApiEndpoint) {
+    const { baseUrl, apiVersion } = this.apiConfig;
+    return `${baseUrl}/${apiVersion}/${endpoint}`;
+  }
+
+  private normalizeQueryParams(params: QueryParams) {
+    return Object.fromEntries(Object.entries(params).filter(([_, value]) => value != null));
+  }
+
+  get<TRawItem>(endpoint: JamendoApiEndpoint, params: QueryParams) {
+    return this.httpClient.get<JamendoResponse<TRawItem>>(this.buildUrl(endpoint), {
+      params: {
+        client_id: this.apiConfig.clientId,
+        ...this.normalizeQueryParams(params),
+      },
+    });
+  }
+}
