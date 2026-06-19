646fdae84e48efebd52cd92546492e596c9ad470
Ina Litvinka <inlibitum@gmail.com>
2026-06-19 21:15:37 +0200

    feat: implement playlist listing with resource API, reset form

--- src/app/pages/library/library.ts
+++ src/app/pages/library/library.ts
@@ -1,22 +1,48 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  signal,
+  inject,
+  resource,
+  viewChild,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Dialog } from '@shared/components/dialog/dialog';
+import { AuthService } from '@core/services/auth/auth-service';
+import { PlaylistApiService } from '@core/services/playlist/playlist-api-service';
+import { Sprite } from '@shared/components/sprite/sprite';
 import { PlaylistCreate } from './components/playlist-create/playlist-create';
 
 @Component({
   selector: 'player-library',
-  imports: [PlaylistCreate, Dialog],
+  imports: [PlaylistCreate, Dialog, RouterLink, Sprite],
   templateUrl: './library.html',
   styleUrl: './library.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [PlaylistApiService],
 })
 export class Library {
+  private authService = inject(AuthService);
+  private playlistApiService = inject(PlaylistApiService);
   protected isModalOpen = signal(false);
 
+  private playlistForm = viewChild<PlaylistCreate>('playlistFormRef');
+
+  protected playlistsResource = resource({
+    loader: async () => {
+      const uid = this.authService.user()?.uid;
+      if (!uid) return [];
+      return await this.playlistApiService.getUserPlaylists(uid);
+    },
+  });
+
   protected openCreateForm() {
+    this.playlistForm()?.resetForm();
     this.isModalOpen.set(true);
   }
 
   protected closeCreateForm() {
     this.isModalOpen.set(false);
+    this.playlistsResource.reload();
   }
 }
