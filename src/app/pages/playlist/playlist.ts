04d4ac566385dec7ba25060847f436a6617aaf7f
Ina Litvinka <inlibitum@gmail.com>
2026-06-20 15:24:00 +0200

    feat: get playlist data

--- src/app/pages/playlist/playlist.ts
+++ src/app/pages/playlist/playlist.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, resource } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { TrackService } from '@core/api/tracks/track-service';
+import { PlaylistApiService } from '@core/services/playlist/playlist-api-service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'player-playlist',
@@ -6,7 +10,27 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   templateUrl: './playlist.html',
   styleUrl: './playlist.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [PlaylistApiService],
 })
 export class Playlist {
+  private trackService = inject(TrackService);
+  private playlistService = inject(PlaylistApiService);
+
   protected id = input.required<string>();
+
+  protected playlistResource = resource({
+    loader: async () => {
+      return await this.playlistService.getPlaylistById(this.id());
+    },
+  });
+
+  protected tracksResource = rxResource({
+    params: () => this.playlistResource.value()?.trackIds ?? [],
+    stream: ({ params: trackIds }) => {
+      if (!trackIds.length) {
+        return of([]);
+      }
+      return this.trackService.getTracksByIds(trackIds);
+    },
+  });
 }
