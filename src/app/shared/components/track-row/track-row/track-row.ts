11e46abdd504a4a39979501d6de4824eb8a7e76e
Ina Litvinka <inlibitum@gmail.com>
2026-06-20 23:08:01 +0200

    feat: add track row component

---
+++ src/app/shared/components/track-row/track-row/track-row.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, input, output, signal } from '@angular/core';
+import { Track } from '@shared/models/track';
+import { TrackCover } from '../../track-cover/track-cover';
+
+@Component({
+  selector: 'player-track-row',
+  imports: [TrackCover],
+  templateUrl: './track-row.html',
+  styleUrl: './track-row.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.playing]': 'isPlaying()',
+    '[class.hovered]': 'isHovered()',
+  },
+})
+export class TrackRow {
+  track = input.required<Track>();
+  index = input.required<number>();
+
+  isPlaying = input(false);
+
+  protected isHovered = signal(false);
+
+  playClicked = output<Track>();
+}
