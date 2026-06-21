6f000e28f9ad8d3e4e0924d4c93a4a12159a219d
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-21 17:34:04 +0300

    feat: implement cacheable track service

---
+++ src/app/core/api/track/track.model.ts
@@ -0,0 +1,22 @@
+import { EntityWithId } from '../common.types';
+
+export interface Track extends EntityWithId {
+  id: string;
+  name: string;
+  artist: {
+    id: string;
+    name: string;
+  };
+  album: {
+    id: string;
+    image: string;
+  };
+  releaseDate: string;
+  image: string;
+  duration: number;
+  plays: number;
+  audio: string;
+  download: string;
+  downloadAllowed: boolean;
+  isFavorite?: boolean;
+}
---
+++ src/app/core/api/track/utils/parse-search-params.ts
@@ -0,0 +1,44 @@
+import { JamendoSearchParams } from '../../jamendo/jamendo.types';
+import { SearchParams } from '../track-service.types';
+
+// NOTE: When you are searching tracks, for instance with 'tags=rock+pop',
+// and at the same time you want to bring the best tracks of the week in advance,
+// you should absolutely use this parameter instead of 'order'
+
+export const parseSearchParams = ({
+  fuzzytags,
+  tags,
+  durationFrom,
+  durationTo,
+  limit,
+  offset,
+  query,
+  sortBy,
+  sortOrder = 'asc',
+}: SearchParams): JamendoSearchParams => {
+  const result: JamendoSearchParams = { limit, offset, search: query.trim() };
+
+  const normalizedTags = tags?.filter(Boolean);
+  const normalizedFuzzyTags = fuzzytags?.filter(Boolean);
+
+  if (normalizedTags?.length) {
+    result.tags = normalizedTags.join('+');
+  } else if (normalizedFuzzyTags?.length) {
+    result.fuzzytags = normalizedFuzzyTags.join('+');
+  }
+
+  const durFrom = ~~Number(durationFrom);
+  const durTo = ~~Number(durationTo);
+
+  if (durTo > 0) {
+    result.durationbetween = `${durFrom < durTo ? durFrom : 0}_${durTo}`;
+  }
+
+  if (sortBy === 'popularity' && (result.tags || result.fuzzytags)) {
+    result.boost = 'popularity_month';
+  } else if (sortBy) {
+    result.order = `${sortBy}_${sortOrder}`;
+  }
+
+  return result;
+};
