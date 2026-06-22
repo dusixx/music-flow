eed75e800077c1fec6ac928f1db9e4841a269b6e
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-22 07:28:58 +0300

    feat: improve cache logic by adding ref counters

--- src/app/core/api/cache/jamendo-cache.ts
+++ src/app/core/api/cache/jamendo-cache.ts
@@ -1,27 +1,26 @@
 import { JamendoResult } from '@api/jamendo/jamendo.types';
 import { EntityWithId } from '@app/core/api/common.types';
 
-type EntityId = string;
-
-interface CacheEntry {
-  data: JamendoResult<EntityId>;
+interface CachePage {
+  data: JamendoResult<string>;
   createdAt: number;
 }
 
 const DEFAULT_TTL = 24 * 60 * 60 * 1000;
 
-export abstract class JamendoCacheService<TEntity extends EntityWithId> {
-  private readonly pages = new Map<string, CacheEntry>();
-  private readonly items = new Map<string, TEntity>();
+export abstract class JamendoCacheService<TItem extends EntityWithId> {
+  private readonly pages = new Map<string, CachePage>();
+  private readonly items = new Map<string, TItem>();
+  private readonly refs = new Map<string, number>();
 
   private _ttl = DEFAULT_TTL;
 
-  private isExpired({ createdAt }: CacheEntry) {
+  private isExpired({ createdAt }: CachePage) {
     return Date.now() - createdAt >= this.ttl;
   }
 
   set ttl(v: number) {
-    const num = ~~v;
+    const num = parseFloat(`${v}`) || 0;
     this._ttl = num < 0 ? Infinity : num;
   }
 
@@ -29,17 +28,25 @@ export abstract class JamendoCacheService<TEntity extends EntityWithId> {
     return this._ttl;
   }
 
-  set(pageId: string, data: JamendoResult<TEntity>) {
+  private addItem(id: string, item: TItem) {
+    this.items.set(id, item);
+    const refsCount = this.refs.get(id) ?? 0;
+    this.refs.set(id, refsCount + 1);
+  }
+
+  set(pageId: string, data: JamendoResult<TItem>) {
     const { results, hasMore } = data;
     const unique = new Set<string>();
     const ids: string[] = [];
+
+    this.remove(pageId);
 
     results.forEach((item) => {
       const { id } = item;
 
       if (!unique.has(id)) {
         unique.add(id);
-        this.items.set(id, item);
+        this.addItem(id, item);
         ids.push(id);
       }
     });
@@ -49,34 +56,53 @@ export abstract class JamendoCacheService<TEntity extends EntityWithId> {
     });
   }
 
-  get(pageId: string): JamendoResult<TEntity> | null {
-    const entry = this.pages.get(pageId);
-
-    if (!entry) {
+  get(pageId: string): JamendoResult<TItem> | null {
+    const page = this.pages.get(pageId);
+    if (!page) {
       return null;
     }
-    const result = {
-      results: entry.data.results.map((id) => this.items.get(id)!),
-      hasMore: entry.data.hasMore,
-    };
-    if (this.isExpired(entry)) {
+    if (this.isExpired(page)) {
       this.remove(pageId);
+      return null;
     }
-    return result;
+    const { results: ids, hasMore } = page.data;
+    const results = ids.map((id) => this.items.get(id)).filter((v) => v != null);
+
+    if (ids.length !== results.length) {
+      this.remove(pageId);
+      return null;
+    }
+    return {
+      results,
+      hasMore,
+    };
   }
 
   has(pageId: string) {
-    return this.pages.has(pageId);
+    const page = this.pages.get(pageId);
+    return !!(page && !this.isExpired(page));
   }
 
   remove(pageId: string) {
     const page = this.pages.get(pageId);
-    page?.data.results.forEach((id) => this.items.delete(id));
-    return this.pages.delete(pageId);
+    if (!page) {
+      return;
+    }
+    page.data.results.forEach((id) => {
+      const refsCount = (this.refs.get(id) ?? 0) - 1;
+      if (refsCount <= 0) {
+        this.items.delete(id);
+        this.refs.delete(id);
+      } else {
+        this.refs.set(id, refsCount);
+      }
+    });
+    this.pages.delete(pageId);
   }
 
   clear() {
     this.pages.clear();
     this.items.clear();
+    this.refs.clear();
   }
 }
