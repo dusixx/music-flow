211a359b6e0d8d9b3579b1936261e7a058d6690c
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-22 20:11:40 +0300

    feat: add popular/releases stores and update discover page

--- src/app/pages/discover/discover.ts
+++ src/app/pages/discover/discover.ts
@@ -1,28 +1,39 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { TrackService } from '@app/core/api/tracks/track-service';
+import { mapTrack } from '@app/core/api/track/track.mapper';
+import { mockTracks } from '@app/core/api/track/mock/track.mock';
+import { Button } from '@app/shared/components/button/button';
 import { CarouselItem } from '@app/shared/components/carousel/carousel-item/carousel-item';
 import { TrackCard } from '@app/shared/components/track-card/track-card';
 import { JAMENDO_GENRES } from '@app/shared/constants';
 import { CarouselSection } from './components/carousel-section/carousel-section';
+import { PopularStore } from './store/popular-store';
+import { ReleasesStore } from './store/realeases-store';
+
+const POPULAR_COUNT = 15;
+const RELEASES_COUNT = 15;
 
 @Component({
   selector: 'player-discover',
-  imports: [TrackCard, CarouselItem, RouterLink, CarouselSection],
+  imports: [TrackCard, CarouselItem, RouterLink, CarouselSection, Button],
+  providers: [PopularStore, ReleasesStore],
   templateUrl: './discover.html',
   styleUrl: './discover.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Discover {
-  private trackService = inject(TrackService);
+  // protected popular = inject(PopularStore);
+  // protected releases = inject(ReleasesStore);
 
-  protected popular = toSignal(this.trackService.getPopular(), {
-    initialValue: [],
-  });
-  protected releases = toSignal(this.trackService.getNewReleases(), {
-    initialValue: [],
-  });
+  protected popular = { results: signal(mockTracks.slice(0, POPULAR_COUNT).map(mapTrack)) };
+  protected releases = {
+    results: signal(mockTracks.slice(0, RELEASES_COUNT).map(mapTrack)),
+  };
+
+  // constructor() {
+  //   this.popular.load({ limit: POPULAR_COUNT });
+  //   this.releases.load({ limit: RELEASES_COUNT });
+  // }
 
   protected genresList = JAMENDO_GENRES;
 
@@ -30,12 +41,12 @@ export class Discover {
     {
       title: 'popular',
       subtitle: 'popular subtitle',
-      tracks: this.popular,
+      tracks: this.popular.results,
     },
     {
       title: 'new releases',
       subtitle: 'releases subtitle',
-      tracks: this.releases,
+      tracks: this.releases.results,
     },
   ];
 }
