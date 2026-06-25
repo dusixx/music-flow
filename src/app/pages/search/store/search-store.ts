0494b4f53137f708a129eb40be9e1f07aab64938
dusixx <113414935+dusixx@users.noreply.github.com>
2026-06-25 20:33:36 +0300

    feat: implement search store using rxRresource

--- src/app/pages/search/store/search-store.ts
+++ src/app/pages/search/store/search-store.ts
@@ -1,42 +1,93 @@
-import { effect, inject, Injectable } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JamendoSearchParams } from '@app/core/api/jamendo/jamendo.types';
-import { SearchService } from '@app/core/services/search/search-service';
-import { JamendoStore } from '../../../core/api/store/jamendo-store';
-import { TrackService } from '../../../core/api/track/track-service';
-// import { SearchParams } from '../../../core/api/track/track-service.types';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
-import { Track } from '../../../core/api/track/track.model';
-import { paramMapToPlainObject } from './search-store.utils';
+import { mapParamMapToSearchParams } from '@app/core/api/jamendo/utils/map-param-map-to-search';
+import { mapSearchParamsToJamendoSearchParams } from '@app/core/api/jamendo/utils/map-search-params-to-jamendo';
+import { TrackService } from '@app/core/api/track/track-service';
+import { Track } from '@app/core/api/track/track.model';
+import { dedupeById } from '@app/shared/utils/object.utils';
 
-const DEFAULT_LIMIT = 20;
+const DEFAULT_LIMIT = 40;
 
 @Injectable()
-export class SearchStore extends JamendoStore<Track, JamendoSearchParams> {
-  private readonly searchService = inject(SearchService);
+export class SearchStore {
   private readonly trackService = inject(TrackService);
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
-  queryParamMap = toSignal(this.route.queryParamMap, {
+  private shouldAppend = true;
+
+  private queryParamMap = toSignal(this.route.queryParamMap, {
     initialValue: this.route.snapshot.queryParamMap,
   });
 
+  searchParams = computed(() => mapParamMapToSearchParams(this.queryParamMap()));
+
+  jamendoSearchParams = computed(() => mapSearchParamsToJamendoSearchParams(this.searchParams()));
+
+  private offset = this.jamendoSearchParams().offset ?? 0;
+  private limit = this.jamendoSearchParams().limit ?? DEFAULT_LIMIT;
+
+  private readonly resource = rxResource({
+    params: () => this.jamendoSearchParams(),
+
+    stream: ({ params }) => {
+      return this.trackService.search({
+        ...params,
+        limit: this.limit,
+        offset: this.offset,
+      });
+    },
+  });
+  results = signal<Track[]>([]);
+  error = this.resource.error;
+  loading = this.resource.isLoading;
+  hasMore = computed(() => this.resource.value()?.hasMore ?? false);
+  totalCount = computed(() => this.resource.value()?.total ?? 0);
+
   constructor() {
-    super();
     effect(() => {
-      const queryParams = paramMapToPlainObject<JamendoSearchParams & { q: string }>(
-        this.queryParamMap()
-      );
-      const query = queryParams.q?.[0] ?? '';
-      if (!query) {
+      const currentPageResults = this.resource.value()?.results;
+      if (!currentPageResults) {
         return;
       }
-      console.debug('query', query);
-      this.load({ search: query, limit: DEFAULT_LIMIT });
+      if (this.shouldAppend) {
+        this.results.update((v) => dedupeById([...v, ...currentPageResults]));
+        this.shouldAppend = false;
+      } else {
+        this.resetOffset();
+        this.results.set(dedupeById(currentPageResults));
+      }
     });
   }
 
-  protected override fetchData(params: JamendoSearchParams) {
-    return this.trackService.search(params);
+  loadMore() {
+    this.shouldAppend = true;
+    this.offset += this.limit;
+    this.navigate();
+  }
+
+  // FIX: double request - with current offset and next with 0
+  resetOffset() {
+    this.offset = 0;
+    this.router.navigate([], {
+      queryParams: {
+        offset: null,
+      } satisfies JamendoSearchParams,
+      queryParamsHandling: 'merge',
+      replaceUrl: true,
+    });
+  }
+
+  navigate(params?: JamendoSearchParams) {
+    this.router.navigate(['/search'], {
+      queryParams: {
+        ...params,
+        limit: this.limit,
+        offset: this.offset || null,
+      } satisfies JamendoSearchParams,
+      queryParamsHandling: 'merge',
+    });
   }
 }
