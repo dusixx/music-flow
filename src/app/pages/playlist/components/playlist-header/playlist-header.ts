a428927bd534d18e814fc50e74385ce4a49cfa54
Ina Litvinka <inlibitum@gmail.com>
2026-06-25 15:26:34 +0200

    feat: render collage covers in playlist header, use PluralTextPipe

--- src/app/pages/playlist/components/playlist-header/playlist-header.ts
+++ src/app/pages/playlist/components/playlist-header/playlist-header.ts
@@ -3,11 +3,12 @@ import { Playlist } from '@shared/models/firestore.model';
 import { Sprite } from '@shared/components/sprite/sprite';
 import { Button } from '@shared/components/button/button';
 import { DurationTextPipe } from '@shared/pipes/duration-text/duration-text-pipe';
+import { PluralTextPipe } from '@shared/pipes/plural-text/plural-text-pipe';
 import { PlaylistMenu } from '../playlist-menu/playlist-menu';
 
 @Component({
   selector: 'player-playlist-header',
-  imports: [Sprite, Button, PlaylistMenu, DurationTextPipe],
+  imports: [Sprite, Button, PlaylistMenu, DurationTextPipe, PluralTextPipe],
   templateUrl: './playlist-header.html',
   styleUrl: './playlist-header.scss',
 })
