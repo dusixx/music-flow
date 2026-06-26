5c61a130f8ccb70ec4a8b6ce071c7f3b10555d1f
Ina Litvinka <inlibitum@gmail.com>
2026-06-27 00:31:46 +0200

    refactor: use shared dropdown menu in playlist header

--- src/app/pages/playlist/components/playlist-menu/playlist-menu.ts
+++ src/app/pages/playlist/components/playlist-menu/playlist-menu.ts
@@ -1,39 +1,14 @@
-import { Component, signal, output } from '@angular/core';
-import { TuiDropdown, TuiDataList } from '@taiga-ui/core';
-import { TuiActiveZone, TuiObscured } from '@taiga-ui/cdk';
+import { Component, output } from '@angular/core';
 import { Sprite } from '@shared/components/sprite/sprite';
+import { ContextMenu } from '@shared/components/context-menu/context-menu';
 
 @Component({
   selector: 'player-playlist-menu',
-  imports: [TuiDropdown, TuiDataList, TuiActiveZone, TuiObscured, Sprite],
+  imports: [Sprite, ContextMenu],
   templateUrl: './playlist-menu.html',
   styleUrl: './playlist-menu.scss',
 })
 export class PlaylistMenu {
   editClicked = output<void>();
   deleteClicked = output<void>();
-
-  protected isDropdownOpen = signal(false);
-
-  protected onDotsClick() {
-    this.isDropdownOpen.update((open) => !open);
-  }
-
-  protected onActiveZone(active: boolean) {
-    if (!active) {
-      this.isDropdownOpen.set(false);
-    }
-  }
-
-  protected onObscured(obscured: boolean) {
-    if (obscured) {
-      this.isDropdownOpen.set(false);
-    }
-  }
-
-  protected onActionClick(type: 'edit' | 'delete') {
-    this.isDropdownOpen.set(false);
-    if (type === 'edit') this.editClicked.emit();
-    if (type === 'delete') this.deleteClicked.emit();
-  }
 }
