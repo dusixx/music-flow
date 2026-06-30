ab9f7ac44e6abed39714e02d2248ca8ae51fe428
Ina Litvinka <inlibitum@gmail.com>
2026-06-30 19:43:00 +0200

    refactor: move playlist-related constants to pages/playlist/

---
+++ src/app/pages/playlist/playlist.const.ts
@@ -0,0 +1,6 @@
+export const MIN_SEARCH_LENGTH = 2;
+export const RECOMMEND_DISPLAY_LIMIT = 5;
+export const MOSAIC_COVERS_COUNT = 4;
+export const DUE_TIME = 500;
+export const RECOMMEND_REQUEST_LIMIT = 10;
+export const RECOMMEND_MAX_OFFSET = 100;
--- src/app/shared/constants/playlist-components.const.ts
+++
@@ -1,4 +0,0 @@
-export const MIN_SEARCH_LENGTH = 2;
-export const RECOMMENDATIONS_LIMIT = 3;
-export const MOSAIC_COVERS_COUNT = 4;
-export const DUE_TIME = 500;
